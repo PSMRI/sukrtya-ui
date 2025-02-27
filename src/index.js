09264a59cd16251c98d95da5d2300483557fe447
chandan_piramal <chandankumar@piramalswasthya.org>
2025-02-27 19:22:50 +0530

    update api

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import axios from 'axios';
 
 // Set the base URL
-//axios.defaults.baseURL = 'http://a9d703198cd42472891b5692724cb600-769551428.ap-south-1.elb.amazonaws.com';
-axios.defaults.baseURL = 'https://sukrtya.api.nitag.in';
+axios.defaults.baseURL = 'https://api.sukrtyaapi.in';
+//axios.defaults.baseURL = 'https://sukrtya.api.nitag.in';
 //axios.defaults.baseURL = 'http://localhost:8080';
 
 // Optional: Set other defaults, like headers
