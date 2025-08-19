0d15c99be94e148cf3622139af274fe08b8ffcf1
Chandan Kumar <chandankumar@piramalswasthya.org>
2025-08-19 22:11:28 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import Profile from "./Pages/Profile";
 
 // Set axios default baseURL from environment variable
 axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL || 'https://sukrityaapi.sangha4u.in';
-
+console.log("API Base URL:", axios.defaults.baseURL);
 // Optional: Set other defaults, like headers
 //axios.defaults.headers.common['Authorization'] = 'Bearer your_token_if_any';
 //axios.defaults.headers.post['Content-Type'] = 'application/json';
